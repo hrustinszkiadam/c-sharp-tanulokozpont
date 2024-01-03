bda5b6e956035e967ca66b82b74a10f0415c92f6
hrustinszkiadam <hrustinszkiadam@gmail.com>
2024-01-03 17:04:06 +0100

    refactor: Simplified 'new' expressions

--- Program.cs
+++ Program.cs
@@ -4,18 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Student john = new Student("John Doe", new DateTime(1997, 9, 5));
-            Student jane = new Student("Jane Doe", new DateTime(2001, 12, 31));
-            Student jack = new Student("Jack Doe", new DateTime(1995, 6, 7));
-            Student jill = new Student("Jill Doe", new DateTime(1999, 3, 21));
-            Student vanessa = new Student("Vanessa Doe", new DateTime(1994, 10, 14));
+            Student john = new("John Doe", new DateTime(1997, 9, 5));
+            Student jane = new("Jane Doe", new DateTime(2001, 12, 31));
+            Student jack = new("Jack Doe", new DateTime(1995, 6, 7));
+            Student jill = new("Jill Doe", new DateTime(1999, 3, 21));
+            Student vanessa = new("Vanessa Doe", new DateTime(1994, 10, 14));
 
-            Course math = new Course("Math", "Mathematics");
-            Course english = new Course("English", "English language");
-            Course physics = new Course("Physics", "Low level physics");
+            Course math = new("Math", "Mathematics");
+            Course english = new("English", "English language");
+            Course physics = new("Physics", "Low level physics");
 
-            Instructor mrSmith = new Instructor("Mr. Smith");
-            Instructor mrWhite = new Instructor("Mr. White");
+            Instructor mrSmith = new("Mr. Smith");
+            Instructor mrWhite = new("Mr. White");
 
 
             Console.ReadKey();
