b637987bb9d11abfd1ea709c076c9476b83f1de4
hrustinszkiadam <hrustinszkiadam@gmail.com>
2024-01-07 21:30:43 +0100

    refactor: Added static lists for class instances

---
+++ Database.cs
@@ -0,0 +1,9 @@
+namespace tanulokozpont
+{
+   static class Database
+   {
+      static List<Student> students = new List<Student>();
+      static List<Instructor> instructors = new List<Instructor>();
+      static List<Course> courses = new List<Course>();
+   }
+}
--- Program.cs
+++ Program.cs
@@ -4,20 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Student john = new("John Doe", new DateTime(1997, 9, 5));
-            Student jane = new("Jane Doe", new DateTime(2001, 12, 31));
-            Student jack = new("Jack Doe", new DateTime(1995, 6, 7));
-            Student jill = new("Jill Doe", new DateTime(1999, 3, 21));
-            Student vanessa = new("Vanessa Doe", new DateTime(1994, 10, 14));
-
-            Course math = new("Math", "Mathematics");
-            Course english = new("English", "English language");
-            Course physics = new("Physics", "Low level physics");
-
-            Instructor mrSmith = new("Mr. Smith");
-            Instructor mrWhite = new("Mr. White");
-
-
+            
             Console.ReadKey();
         }
     }
