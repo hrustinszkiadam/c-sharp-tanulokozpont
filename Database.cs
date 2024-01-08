37c6cd67da4c442306fe374365d72d8d38a9dc63
hrustinszkiadam <hrustinszkiadam@gmail.com>
2024-01-08 20:55:12 +0100

    feat: ChooseIndex()

--- Database.cs
+++ Database.cs
@@ -5,5 +5,18 @@ namespace tanulokozpont
       public static List<Student> students = new();
       public static List<Instructor> instructors = new();
       public static List<Course> courses = new();
+
+      public static int ChooseIndex(string type, int listLength)
+      {
+         Console.Write($"Kérem a {type} sorszámát: ");
+         int index = -1;
+         while (index < 0 || index >= listLength)
+         {
+            Console.Write($"Kérem a {type} sorszámát: ");
+            index = int.Parse(Console.ReadLine() ?? "0") - 1;
+         }
+
+         return index;
+      }
    }
 }
